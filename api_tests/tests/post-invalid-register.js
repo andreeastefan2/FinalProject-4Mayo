03751fb2bfc787dd431fd4344d1ce926ad759c20
Andreea Stefan <andreea_stefan77@yahoo.com>
2023-11-16 01:10:57 +0200

    separate and rename scripts

--- api_tests/tests/post-invalid-register.js
+++ api_tests/tests/post-invalid-register.js
@@ -4,21 +4,23 @@ const { faker } = require("@faker-js/faker");
 const baseUrl = "https://practice.expandtesting.com/notes/api/";
 const randomEmail = faker.internet.email();
 const randomPassword = faker.internet.password();
-const randomUserName = faker.internet.userName();
+const randomUserinvalid = faker.string.sample(3);
 
-describe("POST - Register User ", () => {
+describe("POST - invalid register ", () => {
   before(async () => {
     request.setDefaultTimeout(10000);
   });
 
-  it("Register User - valid scenario", async () => {
+  it("Register User - invalid username", async () => {
     await spec()
       .post(baseUrl + "users/register")
       .withBody({
-        name: randomUserName,
+        name: randomUserinvalid,
         email: randomEmail,
         password: randomPassword,
       })
-      .expectStatus(201);
+      .inspect()
+      .expectStatus(400)
+      .expectBodyContains("User name must be between 4 and 30 characters");
   });
 });
